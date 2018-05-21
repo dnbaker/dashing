137627d21b0de27b2a0657316dfaee672bdc4a0f
dnbh <dnb@cs.jhu.edu>
2018-05-21 13:43:13 -0400

    Slightly more logging.

--- pairselector.py
+++ pairselector.py
@@ -6,12 +6,12 @@ import numpy as np
 def main():
     import argparse
     p = argparse.ArgumentParser()
-    p.add_argument("--npairs", type=int, default=100,
+    p.add_argument("--npairs", type=int, default=20,
                    help=("Number of pairs within bucket to store (to "
                          "account for potential inaccuracies"
                          " in our estimation."))
     p.add_argument("--linspace", "-l",
-                   type=int, default=20,
+                   type=int, default=100,
                    help="Number of buckets between 0 and 1 to cover.")
     p.add_argument("-o", "--outpath", default="/dev/stdout")
     p.add_argument("distfile", help="Path to distance file")
@@ -26,6 +26,7 @@ def main():
 
     def unfilled_bucket_count():
         return sum(map(lambda x: len(x) < p, idxpairs))
+    names = next(open(a.distfile)).strip().split('\t')[1:]
     for line in open(a.distfile):
         if line[0] == '#':
             if not labels:
@@ -49,7 +50,14 @@ def main():
     if unfilled_bucket_count():
         print(f"Unfilled bucket count: {unfilled_bucket_count()}. "
               "Emitting unfinished results.", file=sys.stderr)
+    else:
+        print("No unfilled bucket count any more, we got 'em all.",
+              file=sys.stderr)
     with open(a.outpath, "w") as f:
+        for pairlist in idxpairs:
+            for x, y in pairlist:
+                f.write("%s\t%s\n" % (names[x], names[y]))
+        f.write("=" * 80)
         f.write("#Fraction\tIndices of pairs [0-based]...\n")
         for values, frac in zip(idxpairs, np.linspace(0., 1., lsp + 1)[:-1]):
             f.write("%f\t%s\n" % (frac, '\t'.join(map(str, values))))
