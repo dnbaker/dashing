9b0b9a66c92f9419f65697c84da26a41f0ebf634
Daniel Baker <dnb@cs.jhu.edu>
2019-06-10 23:31:29 -0700

    Pairs (#27)
    * Drafted support for multiple input files per line, separated by spaces. (This can be configured by the FNAME_SEP macro.)
    
    * Add for_each_substr to dashing dist.

---
+++ src/substrs.h
@@ -0,0 +1,27 @@
+#ifndef FOR_EACH_SUBSTR_H__
+#define FOR_EACH_SUBSTR_H__
+#include <string>
+#include <cstring>
+
+
+template<typename F>
+void for_each_substr(const F &func, const std::string &s, const int sep=' ') {
+    const char *p;
+    if((p = std::strchr(s.data(), sep)) == nullptr) {
+        func(s.data());
+        return;
+    }
+    const char *p2 = s.data();
+    std::string tmp(p2, p);
+    do {
+        func(tmp.data());
+        std::swap(p2, ++p);
+        if((p = std::strchr(p2, sep)) == nullptr) {
+            func(p2);
+            break;
+        }
+        tmp = std::string(p2, p);
+    } while(1);
+}
+
+#endif
